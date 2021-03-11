197e2461b6918333419f1844ab49c3d1c2fed501
victorvergoossen <victor.vergoossen@dephion.com>
2021-03-11 15:51:09 +0100

    Code styling

--- src/pages/SearchType.tsx
+++ src/pages/SearchType.tsx
@@ -3,27 +3,6 @@ import { debounce, MuiThemeProvider, TextField } from '@material-ui/core';
 import { createMuiTheme } from '@material-ui/core';
 import { getPokemon, getPokemons } from '../gateways/api-gateway';
 
-const pokeTypes = [
-  'bug',
-  'dark',
-  'dragon',
-  'electric',
-  'fairy',
-  'fighting',
-  'fire',
-  'flying',
-  'ghost',
-  'grass',
-  'ground',
-  'ice',
-  'normal',
-  'poison',
-  'psychic',
-  'rock',
-  'steel',
-  'water',
-];
-
 type PokemonList = {
   name: string,
   url: string;
@@ -71,7 +50,7 @@ const SearchType = ({ doType }: { doType: (newType: string) => void }) => {
   const selectPokemon = (name: string | undefined) => {
     if (!name) return;
     setShowList(false);
-    doType(name.toLowerCase());
+    doType(name);
   };
 
   const onChange = debounce((event: ChangeEvent & { target: { value: string } }) => {
@@ -96,7 +75,6 @@ const SearchType = ({ doType }: { doType: (newType: string) => void }) => {
       });
     };
 
-
     // When user types more than 2 characters, start timeout until api is called
     if (timeOut.current) clearTimeout(timeOut.current);
 
@@ -112,25 +90,23 @@ const SearchType = ({ doType }: { doType: (newType: string) => void }) => {
   }, 150);
 
   return (
-    <div className="fixed left-0 top-0 right-0 max-w-lg mt-8 mx-auto z-20">
+    <div className="absolute left-0 top-0 right-0 max-w-lg mt-8 mx-auto z-20">
       <MuiThemeProvider theme={theme}>
         <TextField onChange={onChange} className="w-full" id="outlined-basic" label="Search Pokémon" variant="outlined" />
       </MuiThemeProvider>
       <div className="grid grid-cols-2 gap-2 bg-gray-900 shadow-lg p-4">
-        {showList && activePokemon.map((pokemon: PokemonList, i: number) => {
-          console.log(pokemon);
-          return (
-            <button
-              onClick={() => selectPokemon(pokemon.type)}
-              key={`pokemon_${i}`}
-              className={`relative p-4 bg-gray-800 hover:bg-gray-700 transition-colors duration-200 col-span-1 ${isEven(i) ? '' : 'col-start-2'}`}
-            >
-              <img src={pokemon.sprite || '#'} className="h-48 w-48 pb-6 pt-6 mx-auto object-contain" alt={pokemon.name} />
-              <span className="block font-dot absolute right-0 bottom-0 left-0 text-center mb-16 capitalize">{pokemon.name}</span>
-              <span className={`block ${pokemon.type} rounded border border-white py-1 w-32 mx-auto`}>{pokemon.type}</span>
-            </button>
-          );
-        })}
+        {showList && activePokemon.map((pokemon: PokemonList, i: number) => (
+          <button
+            onClick={() => selectPokemon(pokemon.type)}
+            key={`pokemon_search_${i}`}
+            className={`relative p-4 bg-gray-800 hover:bg-gray-700 transition-colors duration-200 col-span-1 ${isEven(i) ? '' : 'col-start-2'}`}
+          >
+            {pokemon.sprite && <img src={pokemon.sprite} className="h-48 w-48 pb-6 pt-6 mx-auto object-contain" alt={pokemon.name} />}
+
+            <span className="block font-dot absolute right-0 bottom-0 left-0 text-center mb-16 capitalize">{pokemon.name}</span>
+            <span className={`block ${pokemon.type} rounded border border-white py-1 w-32 mx-auto`}>{pokemon.type}</span>
+          </button>
+        ))}
       </div>
     </div>
   );
